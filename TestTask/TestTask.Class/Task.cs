2d463d4f5b644f593f6f7747251230fee384eed5
EvgeniiNech <nechaev2.evgenii.43@gmail.com>
2022-03-05 19:11:46 +0500

    Добавил архитектуру в проект и модели пользователя, задачи

---
+++ TestTask/TestTask.Class/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Class
+{
+    class Task
+    {
+        public Guid TaskID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Create_Date { get; set; }
+        public DateTime Date_Redact { get; set; }
+        public string Status { get; set; }
+    }
+}
